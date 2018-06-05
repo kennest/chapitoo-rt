24493351bc0bce176408d82287f4548b2e7ec902
kennest <kennyoulai@gmail.com>
2018-06-05 13:18:05 +0000

    first commit

---
+++ src/main/java/com/capsule/chapitoolab/models/Locataire.java
@@ -0,0 +1,88 @@
+package com.capsule.chapitoolab.models;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "locataires")
+public class Locataire {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String nom;
+    private String prenom;
+    private Integer age;
+    private String sexe;
+    private String numero;
+    private String photo;
+
+    @ManyToMany(mappedBy = "abonnes",fetch = FetchType.LAZY)
+    private Set<Agence> favoris;
+
+
+    //GETTERS SETTERS
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Set<Agence> getFavoris() {
+        return favoris;
+    }
+
+    public void setFavoris(Set<Agence> favoris) {
+        this.favoris = favoris;
+    }
+}
